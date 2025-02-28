6e4bbdac913b2d6a69064967c7a94d5b4ca41026
wss <wangsaisai@baiwang.com>
2025-02-28 16:08:57 +0800

    feat: 开发查看提交记录接口

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,5 +1,8 @@
 const Router = require('koa-router');
-const { verifyAuth } = require('../middleware/login.middleware');
+const {
+    verifyAuth,
+    getCommitMessage,
+} = require('../middleware/login.middleware');
 const { create, updateInfo } = require('../controller/user.controller');
 const { verifyUser, handlePassword } = require('../middleware/user.middleware');
 const { upload } = require('../middleware/file.middleware');
@@ -10,5 +13,6 @@ userRouter.post('/register', verifyUser, handlePassword, create);
 
 // 更新用户信息（需要登录），使用默认的 'avatar' 字段名
 userRouter.post('/updateInfo', verifyAuth, upload.single('avatar'), updateInfo);
+userRouter.get('/getCommitMessage', getCommitMessage);
 
 module.exports = userRouter;
